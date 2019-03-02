1c6d487cabd88db10c9c2893488cf0add35258bc
Lim Jeong muk <hackerljm@gmail.com>
2019-03-02 18:57:28 +0900

    3강 프로젝트 세팅까지 정리.

---
+++ src/main/java/io/jmlim/springdatajpastudy/Account.java
@@ -0,0 +1,41 @@
+package io.jmlim.springdatajpastudy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    // @Column 이라는 에노테이션은 생략되어있음. 자동으로 들어감
+    private String username;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
