34a8c1f31be7717ba4e532edb0f103d52b98edf0
Lim Jeong muk <hackerljm@gmail.com>
2019-03-03 00:35:59 +0900

    7. JPA 프로그래밍. Fetch 강의내용 추가. (코멘트)

--- src/main/java/io/jmlim/springdatajpastudy/JpaRunner.java
+++ src/main/java/io/jmlim/springdatajpastudy/JpaRunner.java
@@ -19,12 +19,11 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-      /*  *//***
+        /***
          * 어떤 게시글이 있고 게시글을 저장하거나 삭제할 때
          * 포함되어있는 코멘트도 같이 저장해야 하고 같이 삭제해야 한다고 가정함.
-         *//*
-        Post post = new Post();
+         */
+        /*Post post = new Post();
         post.setTitle("Spring Data JPA 언제 보나...");
 
         Comment comment = new Comment();
@@ -35,9 +34,21 @@ public class JpaRunner implements ApplicationRunner {
         comment1.setComment("곧 보여드릴게요.");
         post.addComment(comment1);*/
 
+        /**
+         연간 관례의 엔티티를 어떻게 가져올 것이냐... 지금(Eager)? 나중에(Lazy)?
+         - @OneToMany의 기본값은 Lazy
+         - @ManyToOne의 기본값은 Eager
+         * */
         Session session = entityManager.unwrap(Session.class);
-        Post post = session.get(Post.class, 1l);
-        session.delete(post);
+       // Post post = session.get(Post.class, 4l);
+        Comment comment = session.get(Comment.class, 5l);
+        System.out.println("==================================");
+        System.out.println(comment.getComment());
+        System.out.println(comment.getPost().getTitle());
+        // System.out.println(post.getTitle());
+        //session.save(post);
+
+        //session.delete(post);
     /*    // Cascade 설정이 없으면 포스트만 저장이 되버림.(코멘트는 저장안됨)
         // 저장할 PERSIST 를 전파하는 옵션 추가. @OneToMany(mappedBy = "post", cascade = CascadeType.PERSIST)
         //  - 코멘트 저장됨.
