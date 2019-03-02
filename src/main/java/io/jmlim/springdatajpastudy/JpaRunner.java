aae7690851ef3730d4fe9c7f580f2ee49eed5399
Lim Jeong muk <hackerljm@gmail.com>
2019-03-02 23:45:09 +0900

    7. JPA 프로그래밍. Cascade.md 강의내용 추가.( Post 와 Comment 설명으로 인해 JpaRunner 내용 지우기 전)

--- src/main/java/io/jmlim/springdatajpastudy/JpaRunner.java
+++ src/main/java/io/jmlim/springdatajpastudy/JpaRunner.java
@@ -14,9 +14,6 @@ import javax.transaction.Transactional;
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-    /**
-     *
-     */
     @PersistenceContext
     EntityManager entityManager;
 
@@ -31,12 +28,28 @@ public class JpaRunner implements ApplicationRunner {
         Study study = new Study();
         study.setName("Spring Data JPA");
 
+        // ============== 여기까지가 Transient 상태(JPA 가 모르는 상태)
+
         // 묶음..
         account.addStudy(study);
 
         Session session = entityManager.unwrap(Session.class);
+        // Save 를 하게되면 Persistent 상태 (JPA 가 관리중인 상태)
         session.save(account);
         session.save(study);
         //entityManager.persist(account);
+
+        // 데이터베이스 가지 않고 캐시에 있는것을 가지고 온다.
+        Account jmlim = session.load(Account.class, account.getId());
+
+        // 객체의 변경사항이 있을땐? 반영을 해준다.
+        // 실행해보면.. 불필요한 셀렉트를 하지 않는다.
+        jmlim.setUsername("Lim Jeong Muk");
+        jmlim.setUsername("Lim Jeong Muk 2");
+        //원래 이름이랑 같아서 update 쿼리가 일어나지 않음..
+        jmlim.setUsername("jmlim");
+        
+        System.out.println("===============================");
+        System.out.println(jmlim.getUsername());
     }
 }
