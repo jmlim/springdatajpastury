0baef7253f123ce09cc22a868f827bbddc5dca3a
Lim Jeong muk <hackerljm@gmail.com>
2019-03-04 08:24:26 +0900

    스프링데이터Common Null 처리하기 실습(스프링 5.0 지원 Null 어노테이션 쓰기 전..)

--- src/main/java/io/jmlim/springdatajpastudy/MyRepository.java
+++ src/main/java/io/jmlim/springdatajpastudy/MyRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.Repository;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 공통 인터페이스 정의 시 @NoRepositoryBean 사용함.
@@ -16,4 +17,14 @@ public interface MyRepository<T, Id extends Serializable> extends Repository<T, 
     List<T> findAll();
 
     long count();
+
+    /**
+     * 리턴되는 값이 Null 이어도 이전처럼 널로 처리하는것이 아닌 이 옵셔널이라는 인터페이스가 제공하는 메소드로 검사를 할 수가 있다.
+     */
+    // <E extends T> Optional<E> findById(Id id);
+
+    /**
+     * 그냥 엔티티 타입을 리턴하게 했을 때 그 값이 없으면?
+     */
+    <E extends T> E findById(Id id);
 }
--- src/test/java/io/jmlim/springdatajpastudy/CommentRepositoryTest.java
+++ src/test/java/io/jmlim/springdatajpastudy/CommentRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,7 +21,7 @@ public class CommentRepositoryTest {
 
     @Test
     public void crud() {
-        Comment comment = new Comment();
+       /* Comment comment = new Comment();
         comment.setComment("Hello Comment 임정묵");
         commentRepository.save(comment);
 
@@ -28,6 +29,25 @@ public class CommentRepositoryTest {
         assertThat(all.size()).isEqualTo(1);
 
         long count = commentRepository.count();
-        assertThat(count).isEqualTo(1);
+        assertThat(count).isEqualTo(1);*/
+
+       /* Optional<Comment> byId = commentRepository.findById(100l);
+        assertThat(byId).isEmpty();*/
+
+        /**옵셔널이 제공하는 여러가지 메소드 써보기 */
+        /** 단일 값을 받아올땐 옵셔널을 사용하는 것을 추천한다. 아름답게 처리할 수 있다.*/
+        // boolean present = byId.isPresent();
+        // Comment comment = byId.orElse(new Comment());
+        // Comment comment = byId.orElseThrow(() -> new IllegalArgumentException());
+
+        //그냥 엔티티 타입을 리턴하게 했을 때 그 값이 없으면?
+        // Comment comment = commentRepository.findById(100l);
+        // 최근엔 이런 null 비교를 쓰지 않는 추세..
+        /*if(comment == null) {
+            throw new IllegalArgumentException();
+        }*/
+
+        List<Comment> comments = commentRepository.findAll();
+        assertThat(comments).isEmpty();
     }
 }
